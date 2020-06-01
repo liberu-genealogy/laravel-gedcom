eec40d9d08347915146b729fe5e3d3bbb348a4db
Alex Mills <alex@asdfx.us>
2020-06-01 08:53:21 +0100

    added Event Model

--- src/Models/Event.php
+++ src/Models/Event.php
@@ -3,19 +3,30 @@
 namespace Asdfx\LaravelGedcom\Models;
 
 use Asdfx\LaravelGedcom\Concerns\AccessesProtectedProperties;
+use Asdfx\LaravelGedcom\Concerns\NotableModel;
 use Asdfx\LaravelGedcom\Contracts\GedcomModel;
-use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use PhpGedcom\Record\Indi\Birt;
+use PhpGedcom\Record\Indi\Buri;
+use PhpGedcom\Record\Indi\Chr;
 use PhpGedcom\Record\Indi\Deat;
+use PhpGedcom\Record\Indi\Even;
 
 class Event extends Model implements GedcomModel
 {
-    use AccessesProtectedProperties;
+    use AccessesProtectedProperties, NotableModel;
 
-    public static function createFromGedcom($record): Model
+    /**
+     * @param Even $record
+     * @return Model|null
+     */
+    public static function createFromGedcom($record): ?Model
     {
         $model = new self();
+        if (get_class($record) === Even::class) {
+            return null;
+        }
+
         if (get_class($record) === Birt::class) {
             $model->type = 'Birth';
         }
@@ -24,8 +35,24 @@ class Event extends Model implements GedcomModel
             $model->type = 'Death';
         }
 
-        $model->place = $model->accessProtected($record->getPlac(), 'plac');
-        $model->date = Carbon::parse($record->getDate());
+        if (get_class($record) === Buri::class) {
+            $model->type = 'Burial';
+        }
+
+        if (get_class($record) === Chr::class) {
+            $model->type = 'Christening';
+        }
+
+        if ($record->getPlac() !== null) {
+            $model->place = $model->accessProtected($record->getPlac(), 'plac');
+        }
+
+        $model->date = $record->getDate();
+
+        foreach($record->getNote() as $note) {
+            $noteData = $model->accessProtected($note, '_note');
+            $model->notes()->create(['notes' => $noteData]);
+        }
 
         $model->save();
 
