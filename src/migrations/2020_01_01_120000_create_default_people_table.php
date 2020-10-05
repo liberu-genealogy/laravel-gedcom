927003087ba3a1245bae471901e2858231b2c41b
Curtis Delicata <curtis.delicata@outlook.com>
2020-10-05 14:16:41 +0100

    Change name of migration to fix genealogy projects

--- src/migrations/2020_01_01_120000_create_default_people_table.php
+++ src/migrations/2020_01_01_120000_create_default_people_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePeopleTable extends Migration
+class CreateDefaultPeopleTable extends Migration
 {
     public function up()
     {
