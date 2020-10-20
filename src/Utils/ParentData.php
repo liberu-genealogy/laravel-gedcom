0820129c7a35b8280963b5670f7d9cdf781fc9a9
punitcis <rahul@intuitionsoftech.com>
2020-10-20 19:47:37 +0530

    changes for DB issue

--- src/Utils/ParentData.php
+++ src/Utils/ParentData.php
@@ -4,6 +4,20 @@ namespace GenealogiaWebsite\LaravelGedcom\Utils;
 
 use GenealogiaWebsite\LaravelGedcom\Models\Family;
 use GenealogiaWebsite\LaravelGedcom\Models\Person;
+use GenealogiaWebsite\LaravelGedcom\Utils\Importer\Anci;
+use GenealogiaWebsite\LaravelGedcom\Utils\Importer\Chan;
+use GenealogiaWebsite\LaravelGedcom\Utils\Importer\Indi\Alia;
+use GenealogiaWebsite\LaravelGedcom\Utils\Importer\Indi\Asso;
+use GenealogiaWebsite\LaravelGedcom\Utils\Importer\Indi\Desi;
+use GenealogiaWebsite\LaravelGedcom\Utils\Importer\Indi\Even;
+use GenealogiaWebsite\LaravelGedcom\Utils\Importer\Indi\Lds;
+use GenealogiaWebsite\LaravelGedcom\Utils\Importer\Indi\Name;
+use GenealogiaWebsite\LaravelGedcom\Utils\Importer\NoteRef;
+use GenealogiaWebsite\LaravelGedcom\Utils\Importer\ObjeRef;
+use GenealogiaWebsite\LaravelGedcom\Utils\Importer\Refn;
+use GenealogiaWebsite\LaravelGedcom\Utils\Importer\SourRef;
+use GenealogiaWebsite\LaravelGedcom\Utils\Importer\Subm;
+use GenealogiaWebsite\LaravelGedcom\Utils\otherFields;
 
 class ParentData
 {
@@ -25,7 +39,7 @@ class ParentData
     public static function getPerson($conn, $individuals, $obje_ids)
     {
         $ParentData = [];
-        foreach ($individuals as $k=>$individual) {
+        foreach($individuals as $k=>$individual){
             $g_id = $individual->getId();
             $name = '';
             $givn = '';
@@ -59,6 +73,7 @@ class ParentData
             $slgc = $individual->getSlgc();
             $chan = $individual->getChan();
             $g_id = $individual->getId();
+        
 
             if (!empty($names)) {
                 $name = current($names)->getName();
@@ -83,17 +98,18 @@ class ParentData
             $rin = $individual->getRin();
             $rfn = $individual->getRfn();
             $afn = $individual->getAfn();
+            
 
             if ($givn == '') {
                 $givn = $name;
             }
             $config = json_encode(config('database.connections.'.$conn));
             $key = [
-                ['name', $name], ['givn', $givn], ['surn', $surn], ['sex', $sex],
-            ];
-            $check = Person::on($conn)->where($key)->first();
-            if (empty($check)) {
-                $value = ['name'=>$name, 'givn'=>$givn, 'surn'=>$surn, 'sex'=>$sex, 'uid'=>$uid, 'rin'=>$rin, 'resn'=>$resn, 'rfn'=>$rfn, 'afn'=>$afn];
+                    ['name',$name],['givn',$givn],['surn',$surn],['sex',$sex],['uid',$uid]
+                ];
+            $check = Person::where($key)->first();
+            if(empty($check)){
+                $value = ['name'=>$name,'givn'=>$givn,'surn'=>$surn,'sex'=>$sex,'uid'=>$uid,'rin'=>$rin,'resn'=>$resn,'rfn'=>$rfn,'afn'=>$afn];
 
                 $ParentData[] = $value;
             }
@@ -101,9 +117,11 @@ class ParentData
             // otherFields::insertOtherFields($conn,$individual,$obje_ids,$person);
         }
 
-        foreach (array_chunk($ParentData, 200) as $chunk) {
-            Person::on($conn)->insert($chunk);
+        foreach (array_chunk($ParentData, 200) as $chunk)
+        {
+          Person::on($conn)->insert($chunk);
         }
-        otherFields::insertOtherFields($conn, $individuals, $obje_ids);
+        otherFields::insertOtherFields($conn,$individuals,$obje_ids);
     }
+
 }
