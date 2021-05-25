5818f5d99a5fe1ddd6cba1a12e484329f0ae4cf4
Curtis Delicata <curtis.delicata@outlook.com>
2021-05-25 22:31:05 +0100

    Fix error handling.

--- src/Utils/FamilyData.php
+++ src/Utils/FamilyData.php
@@ -25,68 +25,69 @@ class FamilyData
     {
         $familyData = [];
         try {
-        foreach ($families as $family) {
-            $g_id = $family->getId();
-            $resn = $family->getResn();
-            $husb = $family->getHusb();
-            $wife = $family->getWife();
+            foreach ($families as $family) {
+                $g_id = $family->getId();
+                $resn = $family->getResn();
+                $husb = $family->getHusb();
+                $wife = $family->getWife();
 
-            // string
-            $nchi = $family->getNchi();
-            $rin = $family->getRin();
+                // string
+                $nchi = $family->getNchi();
+                $rin = $family->getRin();
 
-            // array
-            $subm = $family->getSubm();
-            $_slgs = $family->getSlgs();
+                // array
+                $subm = $family->getSubm();
+                $_slgs = $family->getSlgs();
 
-            $description = null;
-            $type_id = 0;
+                $description = null;
+                $type_id = 0;
 
-            $children = $family->getChil();
-            $events = $family->getAllEven();
-            $_note = $family->getNote();
-            $_obje = $family->getObje();
-            $_sour = $family->getSour();
-            $_refn = $family->getRefn();
+                $children = $family->getChil();
+                $events = $family->getAllEven();
+                $_note = $family->getNote();
+                $_obje = $family->getObje();
+                $_sour = $family->getSour();
+                $_refn = $family->getRefn();
 
-            $chan = $family->getChan();
+                $chan = $family->getChan();
 
-           $husband_id = (isset($husb) ? $husb : 0);
-           $wife_id = (isset($wife) ? $wife : 0);
+                $husband_id = (isset($husb) ? $husb : 0);
+                $wife_id = (isset($wife) ? $wife : 0);
 
-    //        $family = Family::on($conn)->updateOrCreate(
-    //            compact('husband_id', 'wife_id', 'description', 'type_id', 'nchi', 'rin')
-    //        );
+                //        $family = Family::on($conn)->updateOrCreate(
+                //            compact('husband_id', 'wife_id', 'description', 'type_id', 'nchi', 'rin')
+                //        );
 
-            // $value = ['husband_id'=>$husband_id, 'wife_id'=>$wife_id, 'description'=>$description, 'type_id'=>$type_id, 'nchi'=>$nchi, 'rin'=>$rin];
-            // $familydata [] = $value;
-        }
-        // Family::insert($familyData);
+                // $value = ['husband_id'=>$husband_id, 'wife_id'=>$wife_id, 'description'=>$description, 'type_id'=>$type_id, 'nchi'=>$nchi, 'rin'=>$rin];
+                // $familydata [] = $value;
+            }
+            // Family::insert($familyData);
 
-        $key = [
-            ['husband_id', $husband_id], ['wife_id', $wife_id], ['description', $description], ['type_id', $type_id], ['nchi', $nchi],
-            ['rin', $rin]
-        ];
-        $check = Family::on($conn)->where($key)->first();
-        if (empty($check)) {
-            $value = [['husband_id', $husband_id], ['wife_id', $wife_id], ['description', $description], ['type_id', $type_id], ['nchi', $nchi],
-            ['rin', $rin]];
+            $key = [
+                ['husband_id', $husband_id], ['wife_id', $wife_id], ['description', $description], ['type_id', $type_id], ['nchi', $nchi],
+                ['rin', $rin]
+            ];
+            $check = Family::on($conn)->where($key)->first();
+            if (empty($check)) {
+                $value = [['husband_id', $husband_id], ['wife_id', $wife_id], ['description', $description], ['type_id', $type_id], ['nchi', $nchi],
+                    ['rin', $rin]];
 
-            $FamilyData[] = $value;
-        }
-        // $person = Person::on($conn)->updateOrCreate($key,$value);
-        // otherFields::insertOtherFields($conn,$individual,$obje_ids,$person);
+                $FamilyData[] = $value;
+            }
+            // $person = Person::on($conn)->updateOrCreate($key,$value);
+            // otherFields::insertOtherFields($conn,$individual,$obje_ids,$person);
 
 
-foreach (array_chunk($FamilyData, 200) as $chunk) {
-Family::on($conn)->insert($chunk);
-}
-        otherFamRecord::insertFamilyData($conn, $families, $obje_ids, $sour_ids);
-    }
-    }
-            catch (\Exception $e) {
+            foreach (array_chunk($FamilyData, 200) as $chunk) {
+                Family::on($conn)->insert($chunk);
+            }
+            otherFamRecord::insertFamilyData($conn, $families, $obje_ids, $sour_ids);
+
+        } catch (\Exception $e) {
             $error = $e->getMessage();
             return \Log::error($error);
         }
+
+    }
 }
 
