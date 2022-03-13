8a350ff238da2d73c1e26687458ae82def72fefd
Curtis Delicata <curtis.delicata@outlook.com>
2022-03-13 15:56:10 +0000

    Laravel 9 migrations

--- src/migrations/2020_04_10_171030_create_types_table.php
+++ src/migrations/2020_04_10_171030_create_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTypesTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateTypesTable extends Migration
      */
     public function up()
     {
-        Schema::create('types', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('description');
-            $table->integer('is_active');
-            $table->timestamps();
-        });
+//        Schema::create('types', function (Blueprint $table) {
+//            $table->id();
+//            $table->string('name');
+//            $table->string('description');
+//            $table->integer('is_active');
+//            $table->timestamps();
+//        });
     }
 
     /**
@@ -29,6 +29,6 @@ class CreateTypesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('types');
+//        Schema::dropIfExists('types');
     }
 }
