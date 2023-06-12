3ddf560866fd8cc71202f8102073a65b69eb7819
Curtis Delicata <curtis.delicata@outlook.com>
2023-06-12 07:48:36 +0000

    Update to PHP 8.2

--- rector.php
+++ rector.php
@@ -1,8 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-//    $rectorConfig->sets([LevelSetList::UP_TO_PHP_82])
-$rectorConfig->import(LevelSetList::UP_TO_PHP_82);
-    $rectorConfig->paths([__DIR__ . '/src']);
+    $rectorConfig->sets([LevelSetList::UP_TO_PHP_82]);
 };
